f938da75209a48a4143b8e66a8b8be8c3d704b78
charusui <charusuui@gmail.com>
2025-10-08 05:05:48 -0900

    scroll

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1084455846"
+    serviceWorkerVersion: "3723958408"
   }
 });
