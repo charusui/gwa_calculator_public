2613c405b30d8e9df420393dbe40cd892bb0888e
charusui <charusuui@gmail.com>
2025-10-22 05:40:30 -0900

    emphasis on 'your' and formatted term average popup

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2501154879"
+    serviceWorkerVersion: "2922875276"
   }
 });
