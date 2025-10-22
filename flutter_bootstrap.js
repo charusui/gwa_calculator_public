d82c53db73f5c149baf73cfe3946463b24dc66a4
charusui <charusuui@gmail.com>
2025-10-22 04:38:08 -0900

    added theme to goal checker and underline ui balancing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1713705585"
+    serviceWorkerVersion: "3251204155"
   }
 });
