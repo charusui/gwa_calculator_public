e23199996cd66f90db6e81bccf1da501dc826b0a
charusui <charusuui@gmail.com>
2025-10-22 02:54:06 -0900

    synced theme with last page of sem calc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1286715896"
+    serviceWorkerVersion: "2234878890"
   }
 });
