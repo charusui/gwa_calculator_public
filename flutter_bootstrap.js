0af4dd0e2c52cabaea397c1e945dd0778f34720b
charusui <charusuui@gmail.com>
2025-10-22 03:56:23 -0900

    added theme to term calc and bg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2234878890"
+    serviceWorkerVersion: "1713705585"
   }
 });
