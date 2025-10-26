5298a646c2e624421d683cefc8ba5617c216ba5b
charusui <charusuui@gmail.com>
2025-10-26 05:02:24 -0900

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1864614672"
+    serviceWorkerVersion: "2951103638"
   }
 });
